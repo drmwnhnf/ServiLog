f6c95ae7ef850575dfdcbeb6e7284d731800680b
NahlSyareza <availablearner@protonmail.com>
2025-05-18 23:10:24 +0700

    url.js file for axios link

--- ServiLog-Frontend/src/App.jsx
+++ ServiLog-Frontend/src/App.jsx
@@ -1,7 +1,8 @@
 export default function App() {
   return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
-  )
+    <div className="w-screen h-screen bg-gray-500 flex flex-col">
+      <div className="w-full h-[15%] bg-red-600"></div>
+      <h1 className="text-3xl font-bold underline">Hello world!</h1>
+    </div>
+  );
 }
