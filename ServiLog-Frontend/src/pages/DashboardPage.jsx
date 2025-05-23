2ade0e78566550ef9ca62bac1980fc4a8343d860
drmwnhnf <darmawanhaneef@gmail.com>
2025-05-23 20:47:24 +0700

    Plis bener

--- ServiLog-Frontend/src/pages/DashboardPage.jsx
+++ ServiLog-Frontend/src/pages/DashboardPage.jsx
@@ -1,7 +1,7 @@
 import { useState, useEffect, useMemo } from 'react';
 import { useNavigate } from 'react-router-dom';
 import axios from 'axios';
-import { useAuth } from '../contexts/authContext';
+import { useAuth } from '../contexts/AuthContext';
 import VehicleModal from '../components/VehicleModal';
 import Navbar from '../components/Navbar';
 
